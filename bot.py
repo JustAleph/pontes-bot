6cea19a59a7f421174497787545634e0c84efee1
JustAleph <44878908+JustAleph@users.noreply.github.com>
2018-12-21 17:56:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,6 +33,9 @@ async def on_message(message):
             embed.set_footer(text="Pontes bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             await client.send_message(message.author, embed=embed)
             await client.delete_message(message)
+            sent = await client.send_message(message.channel, "Ik heb je een DM gestuurd!")
+            await asyncio.sleep(6)
+            await client.delete_message(sent)
  
         elif message.content.startswith('pon!klassen'):
             embed=discord.Embed(title="dit is de lijst met klassen waar ik het rooster van heb!", url="", color=0x3498db)
@@ -43,6 +46,9 @@ async def on_message(message):
             embed.set_footer(text="Pontes bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             await client.send_message(message.author, embed=embed)
             await client.delete_message(message)
+            sent = await client.send_message(message.channel, "Ik heb je een DM gestuurd!")
+            await asyncio.sleep(6)
+            await client.delete_message(sent)
 
         elif message.content.startswith('pon!H3RB'):
             embed=discord.Embed(title="Klik hier voor het rooster van H3RB", url="https://cdn.discordapp.com/attachments/524321408587661334/525685520043343882/Schermafbeelding_2018-12-21_om_15.05.00.png", color=0x3498db)
@@ -52,5 +58,8 @@ async def on_message(message):
             embed.set_footer(text="Pontes bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             sent = await client.send_message(message.author, embed=embed)
             await client.delete_message(message)
+            sent = await client.send_message(message.channel, "Ik heb je een DM gestuurd!")
+            await asyncio.sleep(6)
+            await client.delete_message(sent)
 
 client.run(os.environ["BOT_TOKEN"])

a965c325ea7a39e8ae5a0b54ed42e51e04ecaa7c
JustAleph <44878908+JustAleph@users.noreply.github.com>
2018-12-21 16:51:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
+import asyncio
+import random
+import time
 
 import os
 
 client = commands.Bot(command_prefix= "pon!")
 client.remove_command('help')
+
+chat_filter = ["PON!KLASSEN", "PON!HELP",]
+bypass_list = []
 
 @client.event
 async def on_ready():
@@ -23,17 +30,18 @@ async def on_message(message):
             embed.add_field(name="pon!klassen", value="om een lijst te krijgen van de klassen waar ik het rooster van kan laten zien", inline=False)
             embed.add_field(name="pon![je klas]", value="om het rooster van je klas te krijgen (als ik het heb).", inline=False)
             embed.add_field(name="pon!help", value="toon dit bericht!", inline=False)
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/440836241928486914/525347169214136321/2Q.png")
             embed.set_footer(text="Pontes bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             await client.send_message(message.channel, embed=embed)
-
+            sent = await client.send_message(message.channel, embed=embed)
+            await asyncio.sleep(3) 
+            await client.delete_message(sent)
+ 
         elif message.content.startswith('pon!klassen'):
               embed=discord.Embed(title="dit is de lijst met klassen waar ik het rooster van heb!", url="", color=0x3498db)
               embed.set_author(name="Pontes bot", icon_url="https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
               embed.add_field(name="H3RB", value="type 'pon!H3RB' voor het rooster van H3RB", inline=False)
               embed.add_field(name="pon!klassen", value="krijg een lijst van alle klassen", inline=False)
               embed.add_field(name="pon!help", value="krijg meer informatie over deze bot", inline=False)
-              embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/440836241928486914/525347169214136321/2Q.png")
               embed.set_footer(text="Pontes bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
               await client.send_message(message.channel, embed=embed)
 
@@ -42,7 +50,6 @@ async def on_message(message):
             embed.set_author(name="Pontes bot", icon_url="https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             embed.add_field(name="pon!klassen", value="krijg een lijst van alle klassen", inline=False)
             embed.add_field(name="pon!help", value="krijg meer informatie over deze bot", inline=False)
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/440836241928486914/525347169214136321/2Q.png")
             embed.set_footer(text="Pontes bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             await client.send_message(message.channel, embed=embed)
 

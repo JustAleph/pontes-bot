f0e8a819458936d471faeb2be99f982413183ecc
JustAleph <44878908+JustAleph@users.noreply.github.com>
2018-12-21 16:10:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ async def on_message(message):
             embed.add_field(name="pon!klassen", value="om een lijst te krijgen van de klassen waar ik het rooster van kan laten zien", inline=False)
             embed.add_field(name="pon![je klas]", value="om het rooster van je klas te krijgen (als ik het heb).", inline=False)
             embed.add_field(name="pon!help", value="toon dit bericht!", inline=False)
+            embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             embed.set_footer(text="Pontes bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             await client.send_message(message.channel, embed=embed)
 
@@ -32,6 +33,7 @@ async def on_message(message):
               embed.add_field(name="H3RB", value="type 'pon!H3RB' voor het rooster van H3RB", inline=False)
               embed.add_field(name="pon!klassen", value="krijg een lijst van alle klassen", inline=False)
               embed.add_field(name="pon!help", value="krijg meer informatie over deze bot", inline=False)
+              embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
               embed.set_footer(text="Pontes bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
               await client.send_message(message.channel, embed=embed)
 
@@ -40,6 +42,7 @@ async def on_message(message):
             embed.set_author(name="Pontes bot", icon_url="https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             embed.add_field(name="pon!klassen", value="krijg een lijst van alle klassen", inline=False)
             embed.add_field(name="pon!help", value="krijg meer informatie over deze bot", inline=False)
+            embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             embed.set_footer(text="Pontes bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             await client.send_message(message.channel, embed=embed)
 
